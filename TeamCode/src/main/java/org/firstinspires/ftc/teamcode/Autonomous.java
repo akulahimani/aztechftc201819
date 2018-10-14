6ac39f3554586d71789c9d899f6e990301a80f6a
Jayanth24 <jayanthgunda@gmail.com>
2018-10-14 19:52:39 -0400

    adding computer vision files

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode;
+
+public class Autonomous {
+
+    public double encoderCounts = 537.6;
+    public double radius = 2*25.4;
+    public double circumference = 2*radius*Math.PI;
+
+
+}
